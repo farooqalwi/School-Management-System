b4ec7f399bfc76c7f3b7b7cda7bd71e41132cad4
Farooq Alwi <farooqalwi313@gmail.com>
2020-03-31 17:59:46 +0500

    Working on ViewStudent Method

---
+++ School Management System/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Management_System
+{
+    class Student
+    {
+        public static string Name { get; set; }
+        public static string FatherName { get; set; }
+        public static string Address { get; set; }
+
+        public static void AddStudent()
+        {
+
+            Console.Write("Student's Name: ");
+            Student.Name = Console.ReadLine();
+            Console.Write("Father's Name: ");
+            Student.FatherName = Console.ReadLine();
+            Console.Write("Address: ");
+            Student.Address = Console.ReadLine();
+            Console.WriteLine("Student added.");
+        }
+
+        public static void ViewStudent()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Father's Name: " + FatherName);
+            Console.WriteLine("Address: " + Address);
+
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("1. Add a student\t2. View a student\t3. Delete a student\t'exit' to close menu");
+
+            string choice;
+            bool exit = false;
+
+            do
+            {
+                Console.Write("Choose an appropriate option:");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Student.AddStudent();
+                        break;
+                    case "2":
+                        Student.ViewStudent();
+                        break;
+                    case "3":
+                        Console.WriteLine("Student deleted.");
+                        break;
+                    case "exit":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Operation");
+                        break;
+                }
+            } while (!exit);
+        }
+    }
+}
